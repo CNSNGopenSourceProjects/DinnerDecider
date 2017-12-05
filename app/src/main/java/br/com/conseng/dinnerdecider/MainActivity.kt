e8182d6a5f6555bd7885df0faa2dfd60373a855e
Fernando Camargo <fernandoantonio.camargo@gmail.com>
2017-12-05 01:30:26 -0200

    Implementação de teste completada

--- app/src/main/java/br/com/conseng/dinnerdecider/MainActivity.kt
+++ app/src/main/java/br/com/conseng/dinnerdecider/MainActivity.kt
@@ -3,13 +3,29 @@ package br.com.conseng.dinnerdecider
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "McDonalds", "Pizza Hut")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = "Burger King"
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFoodIndex = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFoodIndex]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodEditTxt.text.toString()
+            if (newFood.length > 0) {
+                foodList.add(newFood)
+                addFoodEditTxt.text.clear()
+                println(foodList)
+            }
+        }
     }
 }
